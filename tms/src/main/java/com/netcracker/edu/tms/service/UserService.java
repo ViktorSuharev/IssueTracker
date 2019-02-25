3c705fe3ead73516be11db93d2a6e388a26ed94d
Vladislav <specialenglishclassmail@gmail.com>
2019-02-25 19:30:32 +0300

    [USER] dao & service implemented

--- tms/src/main/java/com/netcracker/edu/tms/service/UserService.java
+++ tms/src/main/java/com/netcracker/edu/tms/service/UserService.java
@@ -1,4 +1,10 @@
 package com.netcracker.edu.tms.service;
 
+
+import com.netcracker.edu.tms.model.User;
+
 public interface UserService {
+    boolean register(User user);
+
+    boolean checkLogin(String email, String passsword);
 }
