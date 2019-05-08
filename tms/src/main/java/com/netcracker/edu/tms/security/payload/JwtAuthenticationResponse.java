0922ecfff2387dc638e6d1194d62098267a13637
ViktorSuharev <viktor.suhareff@yandex.ru>
2019-05-08 23:22:02 +0300

    fix final in auth response  dto

--- tms/src/main/java/com/netcracker/edu/tms/security/payload/JwtAuthenticationResponse.java
+++ tms/src/main/java/com/netcracker/edu/tms/security/payload/JwtAuthenticationResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class JwtAuthenticationResponse {
     private String accessToken;
-    private final String tokenType = "Bearer";
+    private String tokenType = "Bearer";
 
     public JwtAuthenticationResponse(String accessToken) {
         this.accessToken = accessToken;
